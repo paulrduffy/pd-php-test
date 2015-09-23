8e4e3cb2b150790d945cf96c9844fa16c7dbf50d
Paul Duffy <paulrduffy@gmail.com>
2015-09-23 12:53:45 -0700

    SDK version change, added AWS test file

---
+++ aws-instance-test.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'vendor/autoload.php';
+$config = array();
+
+echo 'Listing instances in this region: ' . getenv('aws_region') . PHP_EOL;
+
+$config['key'] = getenv('aws_access_key_id');
+$config['secret'] = getenv('aws_secret_access_key');
+$config['region'] = getenv('aws_region');
+$ec2Client = \Aws\Ec2\Ec2Client::factory($config);
+
+$result = $ec2Client->DescribeInstances();
+ 
+$reservations = $result['Reservations'];
+
+//$numinst = count($reservations);
+
+//var_dump($reservations);
+
+//echo 'There are ' . $numinst . ' instances' ;
+
+ $y=1;
+
+echo 'ID,instance_id,instance_type' . PHP_EOL;
+
+foreach ($reservations as $reservation) {
+    $instances = $reservation['Instances'];
+    foreach ($instances as $instance) {
+
+       // if ($instance['State']['Name'] = 'running')
+        //{
+        //echo '---> State: ' . $instance['State']['Name'] . PHP_EOL;
+        
+        echo $y . ',';
+        echo $instance['InstanceId'] . ',';
+        //echo '---> Private Dns Name: ' . $instance['PrivateDnsName'] . PHP_EOL;
+        echo $instance['InstanceType'] . ',' . PHP_EOL;
+        //echo '---> Security Group: ' . $instance['SecurityGroups'][0]['GroupName'] . PHP_EOL;
+        //echo '# is ' . $y . PHP_EOL;
+        $y++;
+    }
+    
+
+}
+
+//echo 'Total instances: ' . ($y - 1) . PHP_EOL;
+//echo 'Array count is: ' . count($reservations) . PHP_EOL;
+?>
