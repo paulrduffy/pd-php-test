13168472ecd91630dd614f96df06368c78439fa5
Paul Duffy <paulrduffy@gmail.com>
2015-09-23 13:01:59 -0700

    Cleanup

--- index.php
+++ index.php
@@ -9,9 +9,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Simple PHP App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
     <body bgcolor="#FF0000">
